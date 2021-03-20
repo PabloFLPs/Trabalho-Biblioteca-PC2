988885bb886b6c4fc78ee062b821e9df6ad3566e
FELPs <pablofelipe1999@hotmail.com>
2021-03-20 18:29:41 -0300

    Adding Biblioteca Java directory

---
+++ Biblioteca/src/Dados/Editora.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dados;
+
+import Documentos.Livro;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ISTERRA
+ */
+public class Editora extends ManipulaLivros{
+    private String nome;
+
+    public Editora(String nome) {
+        this.nome = nome;
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+    
+}
---
+++ Biblioteca/src/Dados/ManipulaLivros.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dados;
+
+import Documentos.Livro;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ISTERRA
+ */
+public abstract class ManipulaLivros {
+    
+    
+}
